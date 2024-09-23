6c43e0eeb05a1f23865c36ac27b9e13e51889c15
Sujeet Kumar <sujitkumar6207@gmail.com>
2024-09-23 17:23:36 +0530

    first commit

---
+++ src/components/LatestCollection.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import Productitem from './Productitem';
+
+const LatestCollection = () => {
+
+    const { products } = useContext(ShopContext);
+    const [latestProducts, setLatestProducts]  = useState([]);
+
+    useEffect(()=> {
+        setLatestProducts(products.slice(0,10))
+    },)
+
+    
+
+  return (
+    <div className='m-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTION'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        </p>
+         
+      </div>
+
+      {/*----------Rendering Product------------ */}
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+            latestProducts.map((item,index)=>(
+                <Productitem  key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+            ))
+        }
+    
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default LatestCollection
